db87a8f0bf6d43274504a0e5e69128d1105c6f6f
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-12-09 22:16:45 -0800

    Fit simple linear regressor to the training set

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -14,3 +14,8 @@ y = dataset.iloc[:, 1].values
 # Splitting the dataset into the training and test datasets
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state=0)
+
+# Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
