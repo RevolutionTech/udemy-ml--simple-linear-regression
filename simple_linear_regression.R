a35611c19c305af4de63ce0dfbb2b708bf105756
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-12-09 22:48:56 -0800

    Use data-preprocessing template
    to prepare simple linear regression in R

---
+++ simple_linear_regression.R
@@ -0,0 +1,11 @@
+# Simple Linear Regression
+
+# Importing the dataset
+dataset = read.csv('Salary_Data.csv')
+
+# Splitting the dataset into the training and test datasets
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio=2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
