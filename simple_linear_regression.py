4943146ae2c0a5b116dc41a71e62c05da9c34213
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-12-09 22:07:04 -0800

    Initial commit: Use data-preprocessing template
    to prepare simple linear regression

---
+++ simple_linear_regression.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Simple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the training and test datasets
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state=0)
